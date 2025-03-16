845165780751974bde0793040697f5ba5b2785bf
Reece D'Souza <rebads25@gmail.com>
2025-03-16 22:29:41 +0530

    Updated Resume and translation for Mobile and Japanese

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 function toggleMenu() {
+  console.log("2");
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
 
-const languageToggle = document.getElementById("languageToggle");
-const translatableElements = document.querySelectorAll(".translatable");
-
-console.log(languageToggle);
-
-languageToggle.addEventListener("change", function () {
-  const isJapanese = this.checked;
-
+function toggleLanguage(num) {
+  console.log("3");
+  const isJapanese =
+    document.getElementsByClassName("languageToggle")[num].checked;
+  const translatableElements = document.querySelectorAll(".translatable");
+  console.log(isJapanese);
   translatableElements.forEach((element) => {
     if (isJapanese) {
-      console.log("Japanese");
       element.textContent = element.dataset.ja;
     } else {
-      console.log("English");
       element.textContent = element.dataset.en;
     }
   });
-});
+}
+
+function toggleLanguageMob() {
+  console.log("1");
+  toggleMenu();
+  toggleLanguage(1);
+}
+
+function downloadResume() {
+  const isJapanese = document.getElementById("languageToggle").checked;
+  isJapanese
+    ? window.open("./assets/Reece_RobertHalf_20250224.pdf")
+    : window.open("./assets/Resume_ATS.pdf");
+}
