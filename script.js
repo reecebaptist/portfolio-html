ec11306ffbd94ec86e252d926db0da9039ea9f4b
Reece D'Souza <rebads25@gmail.com>
2025-03-16 19:11:42 +0530

    Updated all translations

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const languageToggle = document.getElementById("languageToggle");
+const translatableElements = document.querySelectorAll(".translatable");
+
+console.log(languageToggle);
+
+languageToggle.addEventListener("change", function () {
+  const isJapanese = this.checked;
+
+  translatableElements.forEach((element) => {
+    if (isJapanese) {
+      console.log("Japanese");
+      element.textContent = element.dataset.ja;
+    } else {
+      console.log("English");
+      element.textContent = element.dataset.en;
+    }
+  });
+});
